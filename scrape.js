01afa44878aa86c7bc9eb0adfb5ab890a23a57f6
Chris Santamaria <chris@santamaria.me>
2019-07-09 10:39:05 -0400

    Move helper functions to utils.js

--- scrape.js
+++ scrape.js
@@ -10,6 +10,8 @@ const puppeteer = require('puppeteer');
 const Json2csvParser = require('json2csv').Parser;
 const fs = require('fs-extra');
 
+const { waitForSISLoad, getClasses } = require("./utils");
+
 (async () => {
     const browser = await puppeteer.launch({
         headless: !!program.headless,
@@ -61,18 +63,6 @@ const fs = require('fs-extra');
         await page.addScriptTag({ path: require.resolve('jquery') });
         await page.addScriptTag({ path: require.resolve('moment') });
 
-        // Promise-based wait for SIS page loads (loading icon in corner)
-        const waitForSISLoad = () => new Promise(resolve => {
-            // Checks every 300ms if the loading icon is visible
-            const loop = setInterval(() => {
-                const visible = $('#WAIT_win0').is(":visible");
-                if (!visible) {
-                    clearInterval(loop);
-                    resolve();
-                }
-            }, 300);
-        });
-
         // Show intructor button
         await page.click(`#win0divDERIVED_CLASS_S_SHOW_INSTR [type='checkbox']`);
         // Show class name button
@@ -80,56 +70,6 @@ const fs = require('fs-extra');
         // Refresh calendar button
         await page.click(`[id='win0divDERIVED_CLASS_S_SSR_REFRESH_CAL$38$'] [type]`);
         await page.evaluate(waitForSISLoad);
-
-        // Grabs courses for displayed week
-        const getClasses = () => {
-            const classes = [];
-
-            // Storing starting day of the given week (Monday)
-            // so that dates of classes can be calculated relative to it later
-            const weekStartDate = new Date($('#DERIVED_CLASS_S_START_DT').val());
-
-            // Using spread to use HTMLCollection as an array
-            // Excluding first row (table header)
-            const rows = [...$(`[id='win0divDERIVED_CLASS_S_HTMLAREA$0'] tbody`)[0].children].slice(1);
-            // Getting x coordinate positions of each row (janky but best way to later get column number of a cell)
-            const columnPositons = [...rows[0].children].map(header => header.getBoundingClientRect().x);
-
-            for (const row of rows) {
-                let cells = [...row.children]
-                    // Using only cells which are classes (specific background color)
-                    .filter(cell => cell.innerHTML.includes('background-color:rgb(182,209,146)'));
-
-                for (const cell of cells) {
-                    const posX = cell.getBoundingClientRect().x;
-                    // Getting the cell's column number based on the
-                    // previously calculated positions of each column (starts at 0)
-                    const colNum = columnPositons.findIndex(col => col == posX) - 1;
-
-                    const cellContents = cell.children[0].innerHTML.split('<br>');
-                    // Splitting times line and reformatting as valid time string for csv
-                    const times = cellContents[3]
-                        .split(' - ')
-                        .map(time => moment(time, 'h:mma').format('h:mm A'));
-
-                    classes.push({
-                        // Course name (class type)
-                        name: `${cellContents[1]} (${cellContents[2]})`,
-                        // Course number
-                        // Instructor (if present)
-                        description: `
-                            ${cellContents[0]}
-                            ${cellContents[6] ? '\n' + cellContents[6] : ''}
-                        `,
-                        location: cellContents[4],
-                        date: moment(weekStartDate).add(colNum, 'd').format('MM/DD/YYYY'),
-                        startTime: times[0],
-                        endTime: times[1]
-                    });
-                }
-            }
-            return classes;
-        };
 
         let classes = [];
         const weeks = program.weeks || 1;
---
+++ utils.js
@@ -0,0 +1,66 @@
+// Promise-based wait for SIS page loads (loading icon in corner)
+const waitForSISLoad = () => new Promise(resolve => {
+    // Checks every 300ms if the loading icon is visible
+    const loop = setInterval(() => {
+        const visible = $('#WAIT_win0').is(":visible");
+        if (!visible) {
+            clearInterval(loop);
+            resolve();
+        }
+    }, 300);
+});
+
+// Grabs courses for displayed week
+const getClasses = () => {
+    const classes = [];
+
+    // Storing starting day of the given week (Monday)
+    // so that dates of classes can be calculated relative to it later
+    const weekStartDate = new Date($('#DERIVED_CLASS_S_START_DT').val());
+
+    // Using spread to use HTMLCollection as an array
+    // Excluding first row (table header)
+    const rows = [...$(`[id='win0divDERIVED_CLASS_S_HTMLAREA$0'] tbody`)[0].children].slice(1);
+    // Getting x coordinate positions of each row (janky but best way to later get column number of a cell)
+    const columnPositons = [...rows[0].children].map(header => header.getBoundingClientRect().x);
+
+    for (const row of rows) {
+        let cells = [...row.children]
+            // Using only cells which are classes (specific background color)
+            .filter(cell => cell.innerHTML.includes('background-color:rgb(182,209,146)'));
+
+        for (const cell of cells) {
+            const posX = cell.getBoundingClientRect().x;
+            // Getting the cell's column number based on the
+            // previously calculated positions of each column (starts at 0)
+            const colNum = columnPositons.findIndex(col => col == posX) - 1;
+
+            const cellContents = cell.children[0].innerHTML.split('<br>');
+            // Splitting times line and reformatting as valid time string for csv
+            const times = cellContents[3]
+                .split(' - ')
+                .map(time => moment(time, 'h:mma').format('h:mm A'));
+
+            classes.push({
+                // Course name (class type)
+                name: `${cellContents[1]} (${cellContents[2]})`,
+                // Course number
+                // Instructor (if present)
+                description: `
+                            ${cellContents[0]}
+                            ${cellContents[6] ? '\n' + cellContents[6] : ''}
+                        `,
+                location: cellContents[4],
+                date: moment(weekStartDate).add(colNum, 'd').format('MM/DD/YYYY'),
+                startTime: times[0],
+                endTime: times[1]
+            });
+        }
+    }
+    return classes;
+};
+
+module.exports = {
+    waitForSISLoad,
+    getClasses
+};
